d75521e51b261443ff498389eb0fd08962d6659d
ml-frederick <63115519+ml-frederick@users.noreply.github.com>
2022-11-30 23:11:37 -0600

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -1,1 +1,38 @@
+#include "inventory.h"
+using namespace std;
+
+Inventory::Inventory() {
+  ISBN = "";
+  int quantity;
+}
+
+Inventory::Inventory(string ISBN, int quantity, float price) {
+  this->ISBN = ISBN;
+  this->quantity = quantity;
+  this->price = price;
+}
+
+string Inventory::getISBN() {
+  return ISBN;
+}
+
+void Inventory::setISBN(string ISBN) {
+  this -> ISBN = ISBN;
+}
+
+int Inventory::getQuantity(){
+  return quantity;
+}
+
+void Inventory::setQuantity(int quantity){
+  this -> quantity = quantity;
+}
+
+float Inventory::getPrice(){
+  return price;
+}
+
+void Inventory::setPrice(float price){
+  this -> price = price;
+}
 
