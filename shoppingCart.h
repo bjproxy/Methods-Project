e61ba7f354988b94a732a908595afb111de17802
Bob <116595153+HBull21@users.noreply.github.com>
2022-11-30 23:11:14 -0600

    Update shoppingCart.h

--- shoppingCart.h
+++ shoppingCart.h
@@ -1,1 +1,32 @@
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
+
+#include <iostream>
+#include <vector>
+#include "inventory.h"
+
+using namespace std;
+
+class ShoppingCart{
+private:
+  vector<ShoppingCart> cartItems;
+  string email;
+  string pass;
+  string ISBN;
+  string price;
+
+public:
+  ShoppingCart();
+  ShoppingCart(string email, string pass, string ISBN, string price);
+  void additem(string ISBN);
+  bool removeItem(string name);
+  void getTotalCost();
+  void printTotalCost();
+  string getUser(string email);
+  void viewCart();
+  void checkout();
+  float getPrice();
+};
+
+#endif
 
