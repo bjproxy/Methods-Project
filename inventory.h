cd1dad5ccb6a405dfed2042e392ea844fdf9b297
ml-frederick <63115519+ml-frederick@users.noreply.github.com>
2022-11-30 23:10:54 -0600

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -1,1 +1,31 @@
+#include <string>
+#include "book.h"
 
+using std::string;
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+class Inventory 
+{
+  private:
+  string ISBN;
+  int quantity;
+  float price;
+  public:  
+  Inventory();
+  Inventory(string ISBN, int quantity, float price);
+
+  //getters
+  string getISBN();
+  int getQuantity();
+  float getPrice();
+
+  //setters
+  void setISBN(string ISBN);
+  void setQuantity(int quantity);
+  void setPrice(float price);
+
+};
+
+#endif
