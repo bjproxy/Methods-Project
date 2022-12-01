6f5c56c7112d9549dcc9e16153924707f7895f1d
bjproxy <112009871+bjproxy@users.noreply.github.com>
2022-11-30 23:21:35 -0600

    Update user.h

--- user.h
+++ user.h
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+using std::istream;
+using std::stringstream;
+
+using std::string;
+
+#ifndef USER_H
+#define USER_H
+
+class User {
+private:
+  string shipAddress;
+  string cardNum;
+  string cvv;
+  string email;
+  string pass;
+  string newshipAddress;
+  string newPayInfo;
+
+public:
+  User();
+  User(string email, string pass, string shipAddress, string cardNum, string cvv);
+  bool search(string email, string pass);
+
+  void display(vector<User> &users);
+  void twoReadFile(vector<User> &users);
+  // getters
+  string getPurchaseHistory();
+  string getShippingAddress();
+  string getPaymentInformation();
+  string getPass();
+  string getEmail();
+
+  // setters
+  void setPurchaseHistory();
+  void setShippingAddress(string shipAddress);
+  void setPaymentInformation(string cardNum, string cvv);
+  void setPass(string pass);
+  void setEmail(string email);
+  //void display();
+  void changeShip(vector<User> &users);
+  void changePay(vector<User> &users);
+};
+
+#endif
 
